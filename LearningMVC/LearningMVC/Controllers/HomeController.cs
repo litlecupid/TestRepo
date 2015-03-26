6246c4abbc8423898f17d4a9c4ca78948054d72c
litlecupid <litlecupid@gmail.com>
2015-03-26 15:21:12 -0400

    Commit my first MVC project to GITHUB.

---
+++ LearningMVC/LearningMVC/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Diagnostics;
+
+namespace LearningMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /default/
+
+        public ViewResult Index()
+        {
+          
+            return View();
+        }
+
+    }
+}
