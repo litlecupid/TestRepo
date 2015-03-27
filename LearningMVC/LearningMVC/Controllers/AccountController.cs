a2450a14fb456db9ffc11a63523d1f604096d8b5
litlecupid <litlecupid@gmail.com>
2015-03-27 10:29:56 -0400

    Test Commit
    Commit all my Learning MVC changes

---
+++ LearningMVC/LearningMVC/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearningMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Login()
+        {
+            string userName = Model.userName;
+            string passWord = passWord;
+            return View();
+        }
+
+    }
+}
---
+++ LearningMVC/LearningMVC/Views/Home/test.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site1.Master" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+<h2>test</h2>
+
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
