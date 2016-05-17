6049932679d9f84e4e39fc09aaf65c499532847d
litlecupid <litlecupid@gmail.com>
2016-05-17 15:06:28 -0400

    Test

---
+++ CircularBuffer/CircularBuffer/CircularBuffer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+namespace CircularBufferTest
+{
+    class CircularBuffer<T>
+    {
+        private int _startPosition;
+        private int _endPosition;
+        private T[] arrayDoubles;
+
+        //default constructor
+        public CircularBuffer():this(capacity:10)
+        {
+            
+        }
+
+        public CircularBuffer(int capacity)
+        {
+            arrayDoubles = new T[capacity];
+            _startPosition = 0;
+            _endPosition = 0;
+        }
+
+        public void Write(T valueToBeWritten)
+        {
+            arrayDoubles[_endPosition] = valueToBeWritten;
+            _endPosition = (_endPosition + 1)%arrayDoubles.Length;
+            Console.WriteLine(_endPosition);
+            if (_endPosition == _startPosition)
+            {
+                _startPosition = (_startPosition + 1)%arrayDoubles.Length;
+            }
+
+        }
+
+        public T Read()
+        {
+            T result = arrayDoubles[_startPosition];
+            _startPosition = (_startPosition + 1)% arrayDoubles.Length;
+            return result;
+        }
+
+        public int capacity
+        {
+            get { return arrayDoubles.Length; }
+        }
+
+        public bool IsEmpty()
+        {
+            return _endPosition==_startPosition;
+        }
+
+    }
+}
--- CircularBuffer/CircularBuffer/Program.cs
+++ CircularBuffer/CircularBuffer/Program.cs
@@ -2,13 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
+using System.IO;
 
-namespace CircularBuffer
+namespace CircularBufferTest
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            var certificate = Convert.ToBase64String(File.ReadAllBytes(@"C:\Users\rprata\testCert3.pfx"));
+
+            CircularBuffer<Double> cbuff = new CircularBuffer<Double>(capacity: 10);
+            while (true)
+            {
+                var value = 0.0;
+                var input = Console.ReadLine();
+                if (double.TryParse(input, out value))
+                {
+                    cbuff.Write((double)value);
+                    continue;
+
+                }
+                break;
+
+            }
+            while (!cbuff.IsEmpty())
+            {
+                Console.WriteLine(cbuff.Read());
+            }
+            Console.ReadLine();
+
+
+
         }
     }
 }
